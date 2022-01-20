81d1dab7de2e04d8f84347d1ab7e308392d2166b
ahach <91538494+ahach@users.noreply.github.com>
2022-01-20 08:45:10 +0100

    page d'accueil OK

--- front/js/index.js
+++ front/js/index.js
@@ -14,7 +14,7 @@ function getProducts(){
         let description = data[i].description;
         let imageUrl = data[i].imageUrl;
         let id = data[i]._id;
-        //let price = data[i].price;
+        let price = data[i].price;
         let alt = data[i].altTxt;
 
 
@@ -25,14 +25,14 @@ console.log(alt);
 // Création d'une variable avec le contenu HTML 
 let items = `<a href="./product.html?id=${id}">
         <article>
-          <img src="${imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">
+          <img src="${imageUrl}" alt="Lorem ipsum dolor sit amet, Kanap name1">${alt}
           <h3 class="productName">${name}</h3>
           <p class="productDescription">${description}</p>
-          <p>${prix}€ </p>
+          <p class="price">${price}€</p>
         </article>
         </a>`;
 
-        // Modification du DOM avec la valeurs de "items"
+        // Modification du DOM avec la valeurs de "items". On ajoute un + sinon le contenu sera écrasé à chaque tour de boucle
 document.querySelector('.items').innerHTML += items;
 // document.getElementById('items).innerHTML 
 
@@ -44,21 +44,3 @@ document.querySelector('.items').innerHTML += items;
 
 // Appel de la fonction
 getProducts();
-
-/*
-// on commence par requeter l'API pour demander les produits
-fetch("http://localhost:3000/api/products")
-.then(function(res) {
-    if (res.ok) {
-      return res.json();
-    }
-  })
-  .then(function(value) {
-    console.log(value);
-  })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
-
-// on veut parcourir chaque réponse retournée et afficher le nom, la photo et la description du produit sous ID items
-*/
